cbbdc6d74f242a38f0cbc0ad7e71839ea8acdd41
AJ Sunder <alecjsunder@gmail.com>
2019-04-07 13:08:13 -0500

    TwoSum working

--- src/com/ajsunder/leetcode/Solution.java
+++ src/com/ajsunder/leetcode/Solution.java
@@ -10,10 +10,21 @@ package com.ajsunder.leetcode;
 
 public class Solution {
 
+    int[] solution = new int[2];
+
     public int[] twoSum(int[] nums, int target) {
 
-        System.out.println("Hello World! " + target);
 
-        return nums;
+        System.out.println("target = " + target);
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target){
+                    solution[0] = i;
+                    solution[1] = j;
+                }
+            }
+        }
+        return solution;
     }
 }
---
+++ src/com/ajsunder/leetcode/SolutionTest.java
@@ -0,0 +1,11 @@
+package com.ajsunder.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void twoSum() {
+        
+    }
+}
