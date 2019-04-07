835fcb2d746a45d4a049b41e90064208325cb245
AJ Sunder <alecjsunder@gmail.com>
2019-04-07 12:47:45 -0500

    Array output

---
+++ src/com/ajsunder/leetcode/Leetcode.java
@@ -0,0 +1,15 @@
+package com.ajsunder.leetcode;
+
+import java.util.Arrays;
+
+public class Leetcode {
+
+    public static void main(String[] args) {
+        // write your code here
+        int[] intArray = {2, 7, 11, 15};
+
+        Solution twoSum = new Solution();
+        System.out.println(Arrays.toString(twoSum.twoSum(intArray, 9)));
+
+    }
+}
---
+++ src/com/ajsunder/leetcode/Solution.java
@@ -0,0 +1,11 @@
+package com.ajsunder.leetcode;
+
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        System.out.println("Hello World! " + target);
+
+        return nums;
+    }
+}
